a90cd4332801c423864c591b19729a96769803c1
strong-ting <s8527502987@gmail.com>
2021-06-03 21:48:06 +0800

    -update gitginore

--- sim/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1600251382 /home/lin/github/tsmc13_neg.v
-1600322048 /home/lin/github/CIC_2018_univ/sim/testfixture.v
-1600316761 /home/lin/github/CIC_2018_univ/src/LCD_CTRL.v
-1600321989 /home/lin/github/CIC_2018_univ/syn/LCD_CTRL_syn.v
